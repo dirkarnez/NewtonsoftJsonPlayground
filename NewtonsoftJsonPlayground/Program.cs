dffe17581f7e78c7fe07f39097238a26e28cf2d2
dirkarnez <smalldirkalex@gmail.com>
2019-02-22 10:53:37 +0800

    - init

---
+++ NewtonsoftJsonPlayground/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace NewtonsoftJsonPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strArr = JsonConvert.DeserializeObject<List<string>>("[\"243\", \"324\"]");
+            Console.WriteLine(strArr[0]);
+            Console.ReadLine();
+        }
+    }
+}
